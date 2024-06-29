1c4c3bcd2808a9ef3412276a6ebbc459e6f76c35
ban <axaxa@gmail.com>
2024-06-29 22:20:01 +0300

    расширение массива

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -1,36 +1,47 @@
 ﻿#include <iostream>
+#include <vector>
 #include <locale>
-#define N 5
 
-int data[N];
+void Input(std::vector<int>& data) {
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Введите элементы массива (нажмите Enter когда закончите):\n";
 
-void Input() {
-	setlocale(LC_ALL, "Rus");
-	std::cout << "Введите элементы массива: \n";
-	for (int i = 0; i < N; i++) {
-		std::cin >> data[i];
-	}
+    int element;
+    while (std::cin >> element) {
+        data.push_back(element);
+
+        if (std::cin.peek() == '\n') {
+            std::cin.ignore();
+            break;
+        }
+    }
 }
 
-void ShellSort(int data[], int size) {
-	for (int interval = size / 2; interval > 0; interval /= 2) {
-		for (int i = interval; i < size; i += 1) {
-			int temp = data[i];
-			int j;
-			for (j = i; j >= interval && data[j - interval] > temp; j -= interval) {
-				data[j] = data[j - interval];
-			}
-			data[j] = temp;
-		}
-	}
+void ShellSort(std::vector<int>& data) {
+    int size = data.size();
+    for (int interval = size / 2; interval > 0; interval /= 2) {
+        for (int i = interval; i < size; i += 1) {
+            int temp = data[i];
+            int j;
+            for (j = i; j >= interval && data[j - interval] > temp; j -= interval) {
+                data[j] = data[j - interval];
+            }
+            data[j] = temp;
+        }
+    }
 }
 
 int main() {
-	Input();
-	int size = sizeof(data) / sizeof(data[0]);
-	ShellSort(data, size);
-	std::cout << "Отсортированный массив: \n";
-	for (int i = 0; i < size; i++)
-		std::cout << data[i] << " ";
-	std::cout << "\n";
+    std::vector<int> data;
+
+    Input(data);
+    ShellSort(data);
+
+    std::cout << "Отсортированный массив:\n";
+    for (const int& num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
 }
