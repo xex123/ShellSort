9d8c605a6defa7fd44377a8e304d51ac44dfa9d2
ban <axaxa@gmail.com>
2024-06-29 23:07:21 +0300

    меню

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -6,6 +6,7 @@ void ShellSort(std::vector<int>& data) {
     setlocale(LC_ALL, "Rus");
 
     std::cout << "Введите элементы массива (нажмите Enter когда закончите):\n";
+
     int element;
     while (true) {
         if (std::cin >> element) {
@@ -32,18 +33,68 @@ void ShellSort(std::vector<int>& data) {
             data[j] = temp;
         }
     }
+
     std::cout << "\nОтсортированный массив:\n";
 
     for (int& num : data) {
         std::cout << num << " ";
     }
     std::cout << "\n";
+    data.clear();
+
+    int choice = 0;
+    while (choice != 1) {
+        std::cout << "\n1.Выход в меню\n";
+        std::cout << "\nВведите свой выбор: ";
+        std::cin >> choice;
+        switch (choice) {
+        case 1:
+            system("cls");
+            break;
+        default:
+            system("cls");
+            std::cout << "Некорректный выбор. Пожалуйста, введите цифру 1 для выхода в меню.\n\n";
+            break;
+        }
+    }
+}
+
+void Menu(std::vector<int>& data) {
+    setlocale(LC_ALL, "Rus");
+    int choice = 0;
+    while (choice != 4) {
+        std::cout << "Меню:\n";
+        std::cout << "\n1.Ввод элементов массива вручную\n";
+        std::cout << "2.Произвольный массив\n";
+        std::cout << "3.Элементы массива из файла\n";
+        std::cout << "4.Выход\n";
+        std::cout << "\nВведите свой выбор: ";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            ShellSort(data);
+        case 2:
+            system("cls");
+            //ShellSortRand(data);
+            //case 3:
+                //system("cls");
+        case 4:
+            system("cls");
+            break;
+        default:
+            system("cls");
+            std::cout << "Некорректный выбор. Пожалуйста, введите цифру от 1 до 4 для выбора.\n\n";
+            break;
+        }
+    }
 }
 
 int main() {
     std::vector<int> data;
 
-    ShellSort(data);
+    Menu(data);
 
     return 0;
 }
